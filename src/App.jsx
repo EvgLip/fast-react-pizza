5c86067cbb3f1e843e75771fda3b235d24ca2faf
evgLip <evggenlip@gmail.com>
2024-11-30 15:22:15 +0300

    основные маршруты в статике

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from './ui/Home';
 import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
 
 const router = createBrowserRouter(
   [
@@ -12,7 +15,19 @@ const router = createBrowserRouter(
     {
       path: '/menu',
       element: <Menu />,
-    }
+    },
+    {
+      path: '/cart',
+      element: <Cart />,
+    },
+    {
+      path: '/order/new',
+      element: <CreateOrder />,
+    },
+    {
+      path: '/order/:orderId',
+      element: <Order />,
+    },
   ]
 );
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function
+  ()
+{
+  return (
+    <header>
+      <Link to='/'>Пицца с быстрой доставкой</Link>
+      <p>Евгений</p>
+    </header>
+  );
+}
